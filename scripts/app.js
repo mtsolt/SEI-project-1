065c7539a3c651a47b9f7ef07945e824b7fab179
Mike Soltysiak <mtsolt@outlook.com>
2021-05-30 11:37:00 +0100

    save before I completely potentially destroy the code

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 function init() {
 
+  
+
   const gridReb = document.querySelector('#rebelSide .grid-wrapper .grid')
   // this creates const for the rebel side
   const gridImp = document.querySelector('#impSide .grid-wrapper .grid')
@@ -12,6 +14,10 @@ function init() {
   // selects all squares on Imp
   const squareReb = document.querySelectorAll('#rebelSide .grid-wrapper .grid div')
   // selects all squares on Reb
+
+
+
+  // Button selects.
   const tieSelect = document.querySelector('.tieSelect')
 
   const xWingSelect = document.querySelector('.xSelect')
@@ -19,6 +25,8 @@ function init() {
   const falconSelect = document.querySelector('.falconSelect')
 
   const cruiserSelect = document.querySelector('.cruiserSelect')
+
+  const imperialShot = document.querySelector('.computerShoot')
 
 
   const width = 16
@@ -28,6 +36,12 @@ function init() {
 
   let startingShipPosition = Math.floor(Math.random() * square.length)
   console.log('gridSquare', gridReb)
+
+  // function startGame() {
+  //   createGrids()
+
+
+  // }
 
 
   function createGrids() { //this creates both grids. Feel like there must be a way to do this with just one for loop, not sure how just yet though. 
@@ -47,12 +61,12 @@ function init() {
       gridImp.appendChild(cell) //imperial grid
       cells.push(cell)
     }
-    // const squareImp = document.querySelectorAll('#impSide .grid-wrapper .grid .square')
+    // const squareImp = document.querySelectorAll('#impSide .grid-wrapper .grid .square')      <<<<---- To randomly assign 5 squares on imperial side
     // for (let i = 0; i < 5; i++) {
     //   squareImp[startingShipPosition].classList.replace('square', 'ship')
     //   startingShipPosition = Math.floor(Math.random() * squareImp.length)
     // }
-    // const squareReb = document.querySelectorAll('#rebelSide .grid-wrapper .grid .square')
+    // const squareReb = document.querySelectorAll('#rebelSide .grid-wrapper .grid .square')     <<<<<--- to randomly assign 5 sqaures on imperial side. 
     // for (let i = 0; i < 5; i++) {
     //   squareReb[startingShipPosition].classList.replace('square', 'ship')
     //   startingShipPosition = Math.floor(Math.random() * squareReb.length)
@@ -110,7 +124,7 @@ function init() {
 
 
 
-// Deploys the rebel ships
+  // Deploys the rebel ships
   function xWing() {
     for (let i = 0; i < 3; i++) { // ridiculously long code for if any conditions that would cause ships to be partially off grid, or overlapping, to not run. Otherwise place i amount ships.
       const squareReb = document.querySelectorAll('#rebelSide .grid-wrapper .grid div')
@@ -162,22 +176,53 @@ function init() {
 
 
 
-
-  function shoot(event) {
+  // The function for player to shoot.
+  function playerShot(event) {
     console.log('click')
     console.log(event.target, 'event target')
     if (event.target.classList.contains('square')) {
       event.target.classList.replace('square', 'miss')
       console.log('miss')
+      setTimeout(computerShoot, 2000)
     } else {
       event.target.classList.replace('ship', 'hit')
       console.log('hit')
+      // repeat function
     }
+  }
+
+  // The function for the computer to shoot.
 
 
+
+  function computerShoot() {
+    console.log('computer shoots')
+    for (let i = 0; i < 1; i++) {
+      const squareReb = document.querySelectorAll('#rebelSide .grid-wrapper .grid div')
+      startingShipPosition = Math.floor(Math.random() * squareReb.length)
+      if (squareReb[startingShipPosition].classList.contains('square')) {
+        squareReb[startingShipPosition].classList.replace('square', 'miss')
+        console.log('computer miss')
+      } else if (squareReb[startingShipPosition].classList.contains('miss') || squareReb[startingShipPosition].classList.contains('hit')) {
+        i--
+      } else {
+        squareReb[startingShipPosition].classList.replace('ship', 'hit')
+        console.log('Computer hits, it goes again!')
+        setTimeout(computerShoot, 2000)
+      }
+    }
+    // function computerHits(startingShipPosition) {
+    //   console.log('computer shoots')
+    //   for (let i = 0; i < 1; i++) {
+
+
+    // }
+  // }
   }
 
 
+
+  imperialShot.addEventListener('click', computerShoot)
 
   tieSelect.addEventListener('click', tieFighter)
 
@@ -188,10 +233,15 @@ function init() {
   cruiserSelect.addEventListener('click', impCruiser)
 
   gridSquare.forEach(item => {
-    item.addEventListener('click', shoot)
+    item.addEventListener('click', playerShot)
   })
 
   createGrids()
+  // run music
+  // start scroller
+  // 1 or 2 player - dream
+  // player choice imp or rebel - dream
+
 
 
 }
