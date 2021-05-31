90d6daeab5e8db0a6860065ebfaba68bef187481
Mike Soltysiak <mtsolt@outlook.com>
2021-05-31 12:28:54 +0100

    Attempted to make function to add ships to specific spots by specific names, but not working. Going to undo all previous moves back to working state

--- scripts/app.js
+++ scripts/app.js
@@ -9,15 +9,23 @@ function init() {
   const gridReb = document.querySelector('#rebelSide .grid-wrapper .grid')
   // this creates const for the rebel side
   const gridImp = document.querySelector('#impSide .grid-wrapper .grid')
-  // this creastes const for imp ide
+  // this creastes const for imp side
   const gridSquare = document.querySelectorAll('.grid')
   // selects the grids
   const square = document.querySelectorAll('.square')
   // selects all squares
-  // const squareImp = document.querySelectorAll('#impSide .grid-wrapper .grid div')
+  const squareImp = document.querySelectorAll('#impSide .grid-wrapper .grid div')
   // selects all squares on Imp
-  // const squareReb = document.querySelectorAll('#rebelSide .grid-wrapper .grid div')
+  const squareReb = document.querySelectorAll('#rebelSide .grid-wrapper .grid div')
   // selects all squares on Reb
+  const scroller = document.querySelector('#scroller')
+  // selects the scroller
+  // const x1 = document.querySelector('#x1')
+  // const x2 = document.querySelector('#x2')
+  // const x3 = document.querySelector('#x3')
+  // const x4 = document.querySelector('#x4')
+  // const x5 = document.querySelector('#falcon')
+
   const rebelButtonsBox = document.querySelector('#rebel')
   console.log(rebelButtonsBox, 'rebelButtonsBox')
 
@@ -56,19 +64,28 @@ function init() {
   const width = 16
   const cellCount = width * width
   const cells = []
+  
   // this is format of grids
+  const x1StartPosition = 0
+  const x2StartPosition = 27
+  const x3StartPosition = 224
+  const x4StartPosition = 235
+  const x5StartPosition = 101
+
+
+  // let tieFighter[] = 
+
 
   let gridPositiion = Math.floor(Math.random() * square.length)
-  console.log('gridSquare', gridReb)
 
-  // function startGame() {
-  //   createGrids()
+
 
 
   // }
 
   function startGame() {
     startGameButton.style.display = 'none'
+    scroller.innerText = 'Which side will you choose?'
     gridReb.appendChild(chooseRebButton)
     gridImp.appendChild(chooseImpButton)
     centerColumn.appendChild(restartButton)
@@ -83,6 +100,8 @@ function init() {
     setTimeout(createGrids(), 2000)
     setTimeout(placeFalcon(), 10)
     setTimeout(placeXWing(), 10)
+    scroller.innerText = 'Your loyalty to the Emperor will be rewarded Captain. Crush these Rebel scum'
+    // return playerPlaceImp()
   }
 
   function chooseRebel() {
@@ -92,7 +111,23 @@ function init() {
     setTimeout(createGrids(), 2000)
     setTimeout(placeCruiser(), 10)
     setTimeout(placeTieFighter(), 10)
+    scroller.innerText = 'This is Gold Wing standing by. Tell us how you want us deployed Gold Leader'
+    placeX1(x1StartPosition)
+    placeX2(x2StartPosition)
+    placeX3(x3StartPosition)
+    placeX4(x4StartPosition)
+    placeX5(x5StartPosition)
   }
+
+  // function playerPlaceImp() {
+
+  // }
+
+  // function playerPlaceReb() {
+    
+
+  // }
+
 
 
 
@@ -107,7 +142,7 @@ function init() {
 
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
-      console.log('cell', cell)
+      // console.log('cell', cell)
       cell.classList.add('square')
       cell.innerHTML = i
       gridImp.appendChild(cell) //imperial grid
@@ -127,10 +162,10 @@ function init() {
 
 
 
-  // Deploys imperial ships
+  //? Deploys imperial ships randomly when player chooses rebel --------------------------------------------------------------------------------------------------
   // deploy the tie fighters
   function placeTieFighter() {
-    for (let i = 0; i < 3; i++) { // ridiculously long code for if any conditions that would cause ships to be partially off grid, or overlapping, to not run. Otherwise place i amount ships. 
+    for (let i = 0; i < 4; i++) { // ridiculously long code for if any conditions that would cause ships to be partially off grid, or overlapping, to not run. Otherwise place i amount ships. 
       const squareImp = document.querySelectorAll('#impSide .grid-wrapper .grid div')
       gridPositiion = Math.floor(Math.random() * squareImp.length)
       if (gridPositiion % width === 14 || gridPositiion % width === 15 || gridPositiion >= 222 || squareImp[gridPositiion].classList.contains('ship') || squareImp[gridPositiion + 1].classList.contains('ship') || squareImp[gridPositiion + 2].classList.contains('ship') || squareImp[gridPositiion + 16].classList.contains('ship') || squareImp[gridPositiion + 17].classList.contains('ship') || squareImp[gridPositiion + 18].classList.contains('ship') || squareImp[gridPositiion + 32].classList.contains('ship') || squareImp[gridPositiion + 33].classList.contains('ship') || squareImp[gridPositiion + 35].classList.contains('ship')) {
@@ -179,10 +214,10 @@ function init() {
 
 
 
-  // Deploys the rebel ships
+  //? Deploys the rebel ships randomly when player chooses Imperial --------------------------------------------------------------------------------------------------
   // deploy xwings
   function placeXWing() {
-    for (let i = 0; i < 3; i++) { // ridiculously long code for if any conditions that would cause ships to be partially off grid, or overlapping, to not run. Otherwise place i amount ships.
+    for (let i = 0; i < 4; i++) { // ridiculously long code for if any conditions that would cause ships to be partially off grid, or overlapping, to not run. Otherwise place i amount ships.
       const squareReb = document.querySelectorAll('#rebelSide .grid-wrapper .grid div')
       gridPositiion = Math.floor(Math.random() * squareReb.length)
       if (gridPositiion % width === 12 || gridPositiion % width === 13 || gridPositiion % width === 14 || gridPositiion % width === 15 || gridPositiion >= 236 || gridPositiion <= 16 || squareReb[gridPositiion].classList.contains('ship') || squareReb[gridPositiion + 1].classList.contains('ship') || squareReb[gridPositiion + 2].classList.contains('ship') || squareReb[gridPositiion + 3].classList.contains('ship') || squareReb[gridPositiion + 4].classList.contains('ship') || squareReb[gridPositiion + 17].classList.contains('ship') || squareReb[gridPositiion - 15].classList.contains('ship')) {
@@ -227,10 +262,78 @@ function init() {
       }
     }
   }
+  // ? ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+
+  function placeX1(position) {
+    squareReb[position].classList.replace('square', 'ship')
+    squareReb[position + 1].classList.replace('square', 'ship')
+    squareReb[position + 2].classList.replace('square', 'ship')
+    squareReb[position + 3].classList.replace('square', 'ship')
+    squareReb[position + 4].classList.replace('square', 'ship')
+    squareReb[position - 15].classList.replace('square', 'ship')
+    squareReb[position + 17].classList.replace('square', 'ship')
+  }
+  function placeX2(position) {
+    squareReb[position].classList.replace('square', 'ship')
+    squareReb[position + 1].classList.replace('square', 'ship')
+    squareReb[position + 2].classList.replace('square', 'ship')
+    squareReb[position + 3].classList.replace('square', 'ship')
+    squareReb[position + 4].classList.replace('square', 'ship')
+    squareReb[position - 15].classList.replace('square', 'ship')
+    squareReb[position + 17].classList.replace('square', 'ship')
+  }
+  function placeX3(position) {
+    squareReb[position].classList.replace('square', 'ship')
+    squareReb[position + 1].classList.replace('square', 'ship')
+    squareReb[position + 2].classList.replace('square', 'ship')
+    squareReb[position + 3].classList.replace('square', 'ship')
+    squareReb[position + 4].classList.replace('square', 'ship')
+    squareReb[position - 15].classList.replace('square', 'ship')
+    squareReb[position + 17].classList.replace('square', 'ship')
+  }
+  function placeX4(position) {
+    squareReb[position].classList.replace('square', 'ship')
+    squareReb[position + 1].classList.replace('square', 'ship')
+    squareReb[position + 2].classList.replace('square', 'ship')
+    squareReb[position + 3].classList.replace('square', 'ship')
+    squareReb[position + 4].classList.replace('square', 'ship')
+    squareReb[position - 15].classList.replace('square', 'ship')
+    squareReb[position + 17].classList.replace('square', 'ship')
+  }
+  function placeX5(position) {
+    squareReb[position].classList.replace('square', 'ship')
+    squareReb[position - 15].classList.replace('square', 'ship')
+    squareReb[position - 14].classList.replace('square', 'ship')
+    squareReb[position + 1].classList.replace('square', 'ship')
+    squareReb[position + 2].classList.replace('square', 'ship')
+    squareReb[position + 3].classList.replace('square', 'ship')
+    squareReb[position + 4].classList.replace('square', 'ship')
+    squareReb[position + 16].classList.replace('square', 'ship')
+    squareReb[position + 17].classList.replace('square', 'ship')
+    squareReb[position + 18].classList.replace('square', 'ship')
+    squareReb[position + 19].classList.replace('square', 'ship')
+    squareReb[position + 32].classList.replace('square', 'ship')
+    squareReb[position + 33].classList.replace('square', 'ship')
+    squareReb[position + 34].classList.replace('square', 'ship')
+    squareReb[position + 35].classList.replace('square', 'ship')
+    squareReb[position + 36].classList.replace('square', 'ship')
+    squareReb[position + 49].classList.replace('square', 'ship')
+    squareReb[position + 50].classList.replace('square', 'ship')
+  }
+  
+
+
+
+
+
+
+
+  // ? Functions for making shots based on whether player chose rebel or imperial ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  // Function for player choosing rebel
   function playerRebelShot(event) {
-    console.log('click')
+    console.log('click Rebel')
     console.log(event.target, 'event target')
     if (event.target.classList.contains('square')) {
       event.target.classList.replace('square', 'miss')
@@ -242,7 +345,6 @@ function init() {
       // repeat function
     }
   }
-  // 
   function computerImperialShot() {
     console.log('computer shoots')
     for (let i = 0; i < 1; i++) {
@@ -251,6 +353,7 @@ function init() {
       if (squareReb[gridPositiion].classList.contains('square')) {
         squareReb[gridPositiion].classList.replace('square', 'miss')
         console.log('computer miss')
+        return playerRebelShot()
       } else if (squareReb[gridPositiion].classList.contains('miss') || squareReb[gridPositiion].classList.contains('hit')) {
         i--
       } else {
@@ -261,9 +364,9 @@ function init() {
     }
   }
 
-  
+  // Function for player choosing Imperial side. 
   function playerImperialShot(event) {
-    console.log('click')
+    console.log('click Imperial')
     console.log(event.target, 'event target')
     if (event.target.classList.contains('square')) {
       event.target.classList.replace('square', 'miss')
@@ -283,6 +386,7 @@ function init() {
       if (squareImp[gridPositiion].classList.contains('square')) {
         squareImp[gridPositiion].classList.replace('square', 'miss')
         console.log('computer miss')
+        return playerImperialShot()
       } else if (squareImp[gridPositiion].classList.contains('miss') || squareImp[gridPositiion].classList.contains('hit')) {
         i--
       } else {
@@ -292,6 +396,7 @@ function init() {
       }
     }
   }
+  // ?--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -311,9 +416,15 @@ function init() {
 
   xWingSelect.addEventListener('click', placeXWing)
 
-  falconSelect.addEventListener('click', placeFalcon)
+  // falconSelect.addEventListener('click', placeFalcon)
 
   cruiserSelect.addEventListener('click', placeCruiser)
+
+  // x1.addEventListener('click', placeX1)
+  // x2.addEventListener('click', placeX2)
+  // x3.addEventListener('click', placeX3)
+  // x4.addEventListener('click', placeX4)
+  // x5.addEventListener('click', placeX5)
 
   // gridSquare.forEach(item => {
   //   item.addEventListener('click', playerShot)
